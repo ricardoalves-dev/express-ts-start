1a11010879ca2505ec3fe7278e6da3d011dddc80
ricardoalves-dev <ricardoalvesdev@hotmail.com>
2025-08-01 12:49:58 -0300

    feat: adicionar tempo de resposta no health checker

--- src/modules/health/Health.ts
+++ src/modules/health/Health.ts
@@ -2,10 +2,12 @@ export class Health {
   readonly uptime: number;
   readonly message: string;
   readonly date: Date;
+  readonly responseTimeInSeconds: number;
 
   constructor() {
     this.uptime = process.uptime();
     this.message = 'OK';
     this.date = new Date();
+    this.responseTimeInSeconds = process.hrtime()[0];
   }
 }
