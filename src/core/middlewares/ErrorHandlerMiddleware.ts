81f258c6432283aefd989c8270409b8e1eff0833
ricardoalves-dev <ricardoalvesdev@hotmail.com>
2025-08-01 12:44:37 -0300

    fix: ajustar tipagem de ErrorHandlerMiddleware

--- src/core/middlewares/ErrorHandlerMiddleware.ts
+++ src/core/middlewares/ErrorHandlerMiddleware.ts
@@ -1,9 +1,9 @@
-import { IAppMiddleware } from "@core/app";
+import { IAppErrorHandlerMiddleware } from "@core/app";
 import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import { ApiResponseError } from "@core/api-response/ApiResponseError";
 import { HttpStatus } from "@core/http-status/HttpStatus";
 
-export class ErrorHandlerMiddleware implements IAppMiddleware {
+export class ErrorHandlerMiddleware implements IAppErrorHandlerMiddleware {
   name: string = 'ErrorHandlerMiddleware';
   get: ErrorRequestHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
     res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(new ApiResponseError(err));
